0e17b64b6888f6c076a45e2a3be2c0a86afe9e0a
Kojowest <kojowest2012@gmail.com>
2025-11-17 02:41:53 +0000

    javascript basics from net ninja

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,18 @@
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+// points = 50; cannot override a constant keyword variable
+
+var score = 75;
+console.log(score);
+
+// variable names cant take spaces
+// variables can contain letters, underscore or numbers but cant start with a number
+//variable names cant start with a keyword
